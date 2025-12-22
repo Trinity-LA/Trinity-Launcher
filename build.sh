f16c07224ef71ec2e3fd812aecc1544aa9958732
Javier C <66850752+franckey02@users.noreply.github.com>
2025-12-21 20:54:32 -0400

    Add installation commands for Trinity
    Added installation commands to copy binaries and resources.

--- build.sh
+++ build.sh
@@ -9,4 +9,9 @@ qmake6 trinchete.pro
 make 
 mv ./bin/trinchete .
 echo "LISTO YA SE COMPILO CON EXITO o eso creemos"
+sudo cp -rf trinito /usr/local/bin/
+sudo cp -rf trinchete /usr/local/bin/
+sudo cp -rf resources/com.trench.trinity.launcher.svg /usr/share/icons/
+sudo cp -rf resources/com.trench.trinity.launcher.desktop /usr/share/applications/
+echo "trinity instalado"
 
